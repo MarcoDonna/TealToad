74e2f441bec540fdd8a0d81778ea1f620bc1eb05
Marco Donna <marco.donna03@gmail.com>
2023-12-29 17:26:29 +0100

    access data in DataFrame with []

--- lib/teal_toad/data_frame.rb
+++ lib/teal_toad/data_frame.rb
@@ -10,6 +10,8 @@ module TealToad
   #   data_frame = DataFrame.new( data: { first_name: %w[Marco Giovanni], last_name: %w[Some Thing], age: [20, 22] } )
   #
   class DataFrame
+    include Enumerable
+
     attr_reader :headers, :rows
 
     ##
@@ -32,6 +34,65 @@ module TealToad
     def initialize(data: nil, headers: nil)
       @headers = headers
       initialize_data data
+    end
+
+    ##
+    # Yields each row in an hash syntax {column_name: value, ...}.
+    # Allows the use of other Enumerable methods like #map and #inject.
+    #
+    # @todo return DataFrame instead of Hash
+    #
+    def each
+      rows.each { |row| yield(row_to_hash(row)) }
+    end
+
+    ##
+    # Retrieve data stored in the DataFrame.
+    #
+    # @param key [Symbol, Numeric, Range, Array]
+    #
+    # @example Accessing columns
+    #   data = { first_name: %w[Marco Giovanni Alice Marco],
+    #            last_name: %w[Some Thing Else Qwerty],
+    #            age: [20, 22, 24, 32] }
+    #   data_frame = DataFrame.new(data: data)
+    #   data_frame[:first_name] # => Series, containing all first names in the DataFrame.
+    #   data_frame[[:first_name, :age]] # => DataFrame, containing the first names and ages.
+    #
+    # @example Accessing rows
+    #   data = { first_name: %w[Marco Giovanni Alice Marco],
+    #            last_name: %w[Some Thing Else Qwerty],
+    #            age: [20, 22, 24, 32] }
+    #   data_frame = DataFrame.new(data: data)
+    #   data_frame[0] # => DataFrame, containing the records at the specified index of the DataFrame.
+    #   data_frame[0..2] # => DataFrame, containing the records in the given range.
+    #   data_frame[[0, 2]] # => DataFrame, containing the records at the given indexes.
+    #
+    def [](key)
+      case key
+      when Symbol
+        column key
+      when Numeric
+        row key
+      when Range
+        range_rows key
+      when Array
+        case key[0]
+        when Symbol
+          list_columns key
+        when Numeric
+          list_rows key
+        end
+      else
+        raise ArgumentError, 'Invalid key when accessing data in DataFrame'
+      end
+    end
+
+    ##
+    # @return [Numeric] The number of rows in the DataFrame.
+    #
+    def size
+      rows.size
     end
 
     private
@@ -72,6 +133,36 @@ module TealToad
       when Array
         @rows = data
       end
+    end
+
+    def row_to_hash(row)
+      headers.zip(row).to_h
+    end
+
+    def column(column_name)
+      Series[*map { |row| row[column_name] }] if headers.include? column_name
+    end
+
+    def list_columns(column_name_list)
+      column_name_list &= headers
+      data = rows.map do |row|
+        column_name_list.map { |column_name| row_to_hash(row)[column_name] }
+      end
+      DataFrame.new(headers: column_name_list, data: data)
+    end
+
+    def row(index)
+      DataFrame.new(headers: headers, data: [rows[index]]) if (-size..(size - 1)).cover? index
+    end
+
+    def range_rows(range)
+      DataFrame.new(headers: headers, data: rows[range])
+    end
+
+    def list_rows(index_list)
+      index_list = index_list.select { |index| (-size..(size - 1)).cover? index }
+      data = index_list.map { |index| rows[index] }
+      DataFrame.new(headers: headers, data: data)
     end
   end
 end
--- spec/teal_toad/data_frame_spec.rb
+++ spec/teal_toad/data_frame_spec.rb
@@ -58,4 +58,120 @@ describe TealToad::DataFrame do
       end
     end
   end
+
+  context 'when accessing data stored in a DataFrame' do
+    subject :data_frame do
+      hash_data = { first_name: %w[Marco Giovanni Alice Marco],
+                    last_name: %w[Some Thing Else Qwerty],
+                    age: [20, 22, 24, 32] }
+      described_class.new(data: hash_data)
+    end
+
+    context 'when accessing a column' do
+      describe '#[]' do
+        it 'returns a Series' do
+          expect(data_frame[:first_name]).to be_a(TealToad::Series)
+        end
+
+        it 'contains the data in the given column' do
+          expect(data_frame[:first_name]).to eq(TealToad::Series[*%w[Marco Giovanni Alice Marco]])
+        end
+
+        it 'returns nil when the argument passed is not a column name' do
+          expect(data_frame[:invalid]).to be_nil
+        end
+      end
+    end
+
+    context 'when accessing a list of columns' do
+      describe '#[]' do
+        let :expected_result do
+          hash_data = { first_name: %w[Marco Giovanni Alice Marco],
+                        age: [20, 22, 24, 32] }
+          described_class.new(data: hash_data)
+        end
+
+        it 'returns a DataFrame' do
+          expect(data_frame[%i[first_name age]]).to be_a(described_class)
+        end
+
+        it 'returns a DataFrame with only the specified columns' do
+          expect(data_frame[%i[first_name age]].rows).to eq(expected_result.rows)
+        end
+
+        it 'ignores invalid column names' do
+          expect(data_frame[%i[first_name invalid age]].rows).to eq(expected_result.rows)
+        end
+      end
+    end
+
+    context 'when accesing a row' do
+      describe '#[]' do
+        let :expected_result_neg do
+          described_class.new(data: { first_name: ['Alice'], last_name: ['Else'], age: [24] })
+        end
+        let :expected_result_pos do
+          described_class.new(data: { first_name: ['Marco'], last_name: ['Some'], age: [20] })
+        end
+
+        it 'returns a DataFrame' do
+          expect(data_frame[0]).to be_a(described_class)
+        end
+
+        it 'returns the row at the given index with an hash like syntax' do
+          expect(data_frame[0].rows).to eq(expected_result_pos.rows)
+        end
+
+        it 'returns the item starting from the end when the argument is negative' do
+          expect(data_frame[-2].rows).to eq(expected_result_neg.rows)
+        end
+
+        it 'returns nil when the given index is outside the range' do
+          expect(data_frame[50]).to be_nil
+        end
+      end
+    end
+
+    context 'when accessing a range of rows' do
+      describe '#[]' do
+        let :expected_result do
+          hash_data = { first_name: %w[Giovanni Alice],
+                        last_name: %w[Thing Else],
+                        age: [22, 24] }
+          described_class.new(data: hash_data)
+        end
+
+        it 'returns a DataFrame' do
+          expect(data_frame[1..2]).to be_a(described_class)
+        end
+
+        it 'returns a DataFrame containing the rows in the given range' do
+          expect(data_frame[1..2].rows).to eq(expected_result.rows)
+        end
+      end
+    end
+
+    context 'when accessing a list of rows' do
+      describe '#[]' do
+        let :expected_result do
+          hash_data = { first_name: %w[Giovanni Marco],
+                        last_name: %w[Thing Qwerty],
+                        age: [22, 32] }
+          described_class.new(data: hash_data)
+        end
+
+        it 'returns a DataFrame' do
+          expect(data_frame[[1, 3]]).to be_a(described_class)
+        end
+
+        it 'returns a DataFrame containing the rows a the given indexes' do
+          expect(data_frame[[1, 3]].rows).to eq(expected_result.rows)
+        end
+
+        it 'ignores indexes out of range' do
+          expect(data_frame[[1, 3, 8]].rows).to eq(expected_result.rows)
+        end
+      end
+    end
+  end
 end
